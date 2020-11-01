3ed5e359c9f7048d647f551c11774ee7f4c338a0
nohtaegeuk <59813576+nohtaegeuk@users.noreply.github.com>
2020-11-01 23:42:13 +0900

    Add files via upload

--- sweaString1.java
+++ sweaString1.java
@@ -4,28 +4,36 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class sweaString {
+public class sweaString1 {
+
 	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
 		System.setIn(new FileInputStream("String.txt"));
 		Scanner sc = new Scanner(System.in);
 		
-		for(int t = 1; t <= 10; t++) {
+		for (int t = 1; t <= 10; t++) {
 			
 			int N = sc.nextInt();
 			String find = sc.next();
 			String str = sc.next();
 			int count = 0;
 			
+			
 			for (int i = 0; i <= str.length() - find.length(); i++) {
 				
-				String find2 = str.substring(i, i+find.length());
+				String find2 = str.substring(i , i + find.length());
+				
 				if(find2.equals(find)) {
 					count++;
 				}
 				
+				
+				
 			}
 			
-			System.out.println("#" + t + " " + count);			
+			System.out.println("#" + t + " " + count);
+			
 		}
 	}
+
 }

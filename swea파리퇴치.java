f3b5d17889594cb0442372946fd7daade8291254
nohtaegeuk <59813576+nohtaegeuk@users.noreply.github.com>
2020-12-09 20:30:19 +0900

    Add files via upload

---
+++ swea파리퇴치.java
@@ -0,0 +1,52 @@
+﻿import java.util.Scanner;
+import java.util.Arrays;
+
+public class 파리퇴치 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			int[][] map = new int[N][N];
+			int[][] tmp = new int[N][N];
+			
+			for (int i = 0; i < map.length; i++) {
+				for (int j = 0; j < map.length; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
+			
+			
+			for (int i = 0; i < map.length; i++) {
+				for (int j = 0; j < map.length; j++) {
+					
+					for (int p = i; p < i+M; p++) {
+						for (int q = j; q < j+M; q++) {
+							
+							if(p >= 0 && q >= 0 && p < N & q < N) {
+								tmp[i][j] += map[p][q];
+								//System.out.println(tmp[i][j]);
+							}
+						}
+					}
+				}
+			}
+			
+			int max = tmp[0][0];
+			
+			for (int i = 0; i < tmp.length; i++) {
+				for (int j = 0; j < tmp.length; j++) {
+					max = Math.max(max, tmp[i][j]);
+				}
+			}
+			
+			System.out.println("#" + tc + " " + max);
+		}
+	}
+
+}
